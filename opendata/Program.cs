4309fc9a76cc3b4656ed1f004b98107b2f9a48fd
Maxou305 <maxime.chazard@le-campus-numerique.fr>
2023-12-22 13:11:25 +0100

    TransportLine class added, REST for API ok

--- opendata/Program.cs
+++ opendata/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +14,36 @@ namespace opendata
     {
         static void Main(string[] args)
         {
-           
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
+            WebRequest request = WebRequest.Create("http://data.mobilites-m.fr/api/linesNear/json?x=5.7317390&y=45.1847290&dist=100&details=true");
+            WebResponse response = request.GetResponse();
+
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+            
+            string responseString = reader.ReadToEnd();
+            List<TransportLine> json = JsonConvert.DeserializeObject<List<TransportLine>>(responseString);
+            Console.WriteLine(json[0].id);
+
+
+
+            //Char[] read = new Char[256];
+
+            ////Read 256 charcters at a time.    
+            //int count = reader.Read(read, 0, 256);
+            //while (count > 0)
+            //{
+            //    // Dump the 256 characters on a string and display the string onto the console.
+            //    String str = new String(read, 0, count);
+            //    Console.Write(str);
+            //    count = reader.Read(read, 0, 256);
+            //}
+            Console.WriteLine();
+
+            reader.Close();
+            stream.Close();
+            response.Close();
         }
     }
 }
---
+++ opendata/TransportLine.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opendata
+{
+    public class TransportLine
+
+    
+    {
+        [JsonProperty("id")]
+        public string id { get; set; }
+
+        [JsonProperty("name")]
+        public string name { get; set; }
+
+        [JsonProperty("lon")]
+        public string lon { get; set; }
+
+        [JsonProperty("lat")]
+        public string lat { get; set; }
+
+        [JsonProperty("lines")]
+        public List<string> lines { get; set; }
+    
+    public TransportLine() { }
+    }
+}
